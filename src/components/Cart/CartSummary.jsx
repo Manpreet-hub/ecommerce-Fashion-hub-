f9ab2b5bb2e0009c6ae0186c252605279916d865
Manpreet <manpreetisar@gmail.com>
2022-04-08 16:52:46 +0530

    update  className

--- src/components/Cart/CartSummary.jsx
+++ src/components/Cart/CartSummary.jsx
@@ -14,35 +14,35 @@ const CartSummary = () => {
   const finalAmt = totalAmt + deliveryCharges;
 
   return (
-    <div class="billing-details">
+    <div className="billing-details">
       <h2>PRICE DETAILS</h2>
       <hr />
-      <div class="v-space-1rem"></div>
-      <div class="flex-row space-between">
-        <span class="para-md">Price ({totalItem} items) </span>
+      <div className="v-space-1rem"></div>
+      <div className="flex-row space-between">
+        <span className="para-md">Price ({totalItem} items) </span>
         <span>{totalAmt}</span>
       </div>
-      <div class="v-space-1rem"></div>
-      <div class="flex-row space-between">
-        <span class="para-md">Delivery charges</span>
+      <div className="v-space-1rem"></div>
+      <div className="flex-row space-between">
+        <span className="para-md">Delivery charges</span>
         {cart.length === 0 ? <span></span> : <span>{deliveryCharges}</span>}
       </div>
-      <div class="v-space-1rem"></div>
+      <div className="v-space-1rem"></div>
       <hr />
-      <div class="v-space-1rem"></div>
-      <div class="flex-row space-between">
-        <span class="para-md">TOTAL AMOUNT</span>
+      <div className="v-space-1rem"></div>
+      <div className="flex-row space-between">
+        <span className="para-md">TOTAL AMOUNT</span>
         {cart.length !== 0 && <span>{finalAmt}</span>}
       </div>
-      <div class="v-space-1rem"></div>
+      <div className="v-space-1rem"></div>
       {cart.length === 0 ? (
-        <span class="para-md"></span>
+        <span className="para-md"></span>
       ) : (
-        <span class="para-md">You will save Rs.999 on this order</span>
+        <span className="para-md">You will save Rs.999 on this order</span>
       )}
-      <div class="v-space-1rem"></div>
+      <div className="v-space-1rem"></div>
 
-      <button class="btn-default btn-primary-solid cart-btn">
+      <button className="btn-default btn-primary-solid cart-btn">
         Place order
       </button>
     </div>
