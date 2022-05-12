ba7242e51eab1511f134fcc3e5e62ac2011aef97
Manpreet <manpreetisar@gmail.com>
2022-05-13 03:17:29 +0530

    [fix] cartItem & wishlist

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -10,29 +10,20 @@ const CartItem = ({ item }) => {
     <>
       {cart.map((item) => {
         return (
-          <div className="horizontal-card  product-card">
+          <div className="horizontal-card product-card">
             <img
               className="card-img product-img"
               src={item.img}
               alt="product-img"
             />
             <div className="card-body">
-              <h4 className="card-title"> {item.title}</h4>
-              <p>₹{item.price}</p>
-              <div className="quantity">
+              <h4 className="card-title card-text"> {item.title}</h4>
+              <p className="card-text">₹{item.price}</p>
+              <div className="quantity card-text">
                 <p>
                   <label>Quantity:</label>
                 </p>
                 <div className="flex-row">
-                  <button
-                    className="plus-minus"
-                    onClick={() =>
-                      cartDispatch({ type: "INCREASE_QTY", payload: item._id })
-                    }
-                  >
-                    +
-                  </button>
-                  <input className="qty-input" type="number" /> {item.qty}
                   <button
                     disabled={item.qty == 1 ? true : false}
                     className="plus-minus"
@@ -41,6 +32,15 @@ const CartItem = ({ item }) => {
                     }
                   >
                     -
+                  </button>
+                  <input className="qty-input" value={item.qty} />
+                  <button
+                    className="plus-minus"
+                    onClick={() =>
+                      cartDispatch({ type: "INCREASE_QTY", payload: item._id })
+                    }
+                  >
+                    +
                   </button>
                 </div>
               </div>
@@ -53,16 +53,38 @@ const CartItem = ({ item }) => {
               >
                 Remove from cart
               </button>
-              <button
-                className="btn-default btn-dark cart-btn"
-                onClick={() => {
-                  wishlistDispatch({ type: "ADD_TO_WISHLIST", payload: item });
-                  cartDispatch({ type: "REMOVE_FROM_CART", payload: item._id });
-                }}
-              >
-                Move to wishlist
-              </button>
-            </div>{" "}
+              {wishlistState.wishlist.some(
+                (cartItem) => cartItem._id === item._id
+              ) ? (
+                <button
+                  className="btn-default btn-dark cart-btn"
+                  onClick={() => {
+                    wishlistDispatch({
+                      type: "REMOVE_FROM_WISHLIST",
+                      payload: item._id,
+                    });
+                  }}
+                >
+                  Remove from wishlist
+                </button>
+              ) : (
+                <button
+                  className="btn-default btn-dark cart-btn"
+                  onClick={() => {
+                    wishlistDispatch({
+                      type: "ADD_TO_WISHLIST",
+                      payload: item,
+                    });
+                    cartDispatch({
+                      type: "REMOVE_FROM_CART",
+                      payload: item._id,
+                    });
+                  }}
+                >
+                  Move to wishlist
+                </button>
+              )}
+            </div>
           </div>
         );
       })}
