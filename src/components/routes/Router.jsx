b4e8e6de7360d7203bd2921d69d95889709c4d1f
Manpreet <manpreetisar@gmail.com>
2022-06-11 19:19:43 +0530

    added protected routes & toast

--- src/components/routes/Router.jsx
+++ src/components/routes/Router.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import {
   Home,
   Login,
@@ -7,18 +7,45 @@ import {
   Cart,
   WishlistProducts,
 } from "../../pages/";
+import { useAuth } from "../../contexts";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const ProtectedRoutes = ({ children }) => {
+  const { authState } = useAuth();
+  const { isAuthenticated } = authState;
+  return isAuthenticated ? children : <Navigate to="/login" replace />;
+};
 
 const Router = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/wishlist" element={<WishlistProducts />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/cart"
+          element={
+            <ProtectedRoutes>
+              <Cart />
+            </ProtectedRoutes>
+          }
+        ></Route>
+        <Route
+          path="/wishlist"
+          element={
+            <ProtectedRoutes>
+              <WishlistProducts />
+            </ProtectedRoutes>
+          }
+        />
+      </Routes>
+      <ToastContainer />
+    </>
   );
 };
 
-export { Router };
+export { Router, ProtectedRoutes };
